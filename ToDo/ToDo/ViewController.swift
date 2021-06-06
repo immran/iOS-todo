4edcd9cb0837e7c28181c5f891f23546ed68a987
Immran Fareedh <immranfareedhs@gmail.com>
2021-06-06 23:06:07 +0530

    Added login UI and Firebase pods

--- ToDo/ToDo/ViewController.swift
+++ ToDo/ToDo/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func loginButton(_ sender: Any) {
+        doLogin()
+    }
+    
+    private func doLogin(){
+        let email = emailTextField.text
+        let pwd = passwordTextField.text
+        
+        print("Its working...\(email) pwd: \(pwd) ")
+    }
 }
 
